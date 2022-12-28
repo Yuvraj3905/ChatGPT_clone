ddf2fba873c74174c99b172f6008c19545d051e8
yuvraj3905 <yuvraj202001@gmail.com>
2022-12-28 13:21:36 +0530

    APi key  by NodeJS
    Using OpenApi Documentation

---
+++ index.js
@@ -0,0 +1,9 @@
+// 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
+
+import { Configuration, OpenAIApi } from 'openai';
+const configuration = new Configuration({
+  organization: 'org-b6C5lzjfuBUnMhXGG6FrvYWf',
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines();
